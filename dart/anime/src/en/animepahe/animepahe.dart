130b9b7a3c10b02d50c5527ab19a7a7e6bdb673c
j88111267 <j88111267@gmail.com>
2025-09-10 16:41:20 -0700

    Add better anime images in animepahe

--- dart/anime/src/en/animepahe/animepahe.dart
+++ dart/anime/src/en/animepahe/animepahe.dart
@@ -30,15 +30,19 @@ class AnimePahe extends MProvider {
     final hasNextPage = jsonResult["current_page"] < jsonResult["last_page"];
     List<MManga> animeList = [];
     for (var item in jsonResult["data"]) {
+      var res1 = (await client.get(
+        Uri.parse("$baseUrl/anime/${item["anime_session"]}?anime_id=${item["session"]}"),
+        headers: headers,
+      )).body;
+      document = parseHtml(res1);
       MManga anime = MManga();
       anime.name = item["anime_title"];
-      anime.imageUrl = item["snapshot"];
+      anime.imageUrl = document.selectFirst("div.anime-poster a").attr("href");
+      anime.description = "$baseUrl/anime/${item["anime_session"]}?anime_id=${item["session"]}";
       anime.link = "/anime/?anime_id=${item["id"]}&name=${item["anime_title"]}";
       anime.artist = item["fansub"];
       animeList.add(anime);
     }
-    return MPages(animeList, hasNextPage);
-  }
 
   @override
   Future<MPages> search(String query, int page, FilterList filterList) async {
@@ -71,7 +75,7 @@ class AnimePahe extends MProvider {
       Uri.parse("$baseUrl/anime/$session?anime_id=$id"),
       headers: headers,
     )).body;
-    final document = parseHtml(res);
+    final document = parseHtml();
     final status =
         (document.xpathFirst('//div/p[contains(text(),"Status:")]/text()') ??
                 "")
